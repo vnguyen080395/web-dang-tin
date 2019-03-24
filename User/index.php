62ed262c3fb949d9942803b31da89322c63c94c7
philong67 <48799932+philong67@users.noreply.github.com>
2019-03-24 09:08:45 -0700

    bnm

--- User/index.php
+++ User/index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<title>Trang chủ</title>
+	<title>Trang chủuuuuuu</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css">
