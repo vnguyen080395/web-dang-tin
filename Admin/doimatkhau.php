c8f30e3aa4937036feb24674e25fc9e5a8ad745b
philong67 <48799932+philong67@users.noreply.github.com>
2019-03-24 08:45:04 -0700

    Update doimatkhau.php

--- Admin/doimatkhau.php
+++ Admin/doimatkhau.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,7 +15,7 @@
     <!-- MetisMenu CSS -->
     <link href="css/metisMenu.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
+
     <link href="css/startmin.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
